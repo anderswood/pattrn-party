e83b7036e38e56a19f77e93f54c8fcbd5080fd5a
Anders Wood <anderswood@gmail.com>
2017-07-22 14:50:42 -0600

    wrap it up!

--- script.js
+++ script.js
@@ -9,41 +9,58 @@
 
 $('.tab').on('click', function() {
   // Update tabs
+  let activeStatus = $(this).hasClass('active-tab-mbl');
   let allTabElements = $(this).closest('.accordian-container').children('.tab');
 
-  allTabElements.removeClass('active-tab');
-  allTabElements.addClass('inactive-tab');
+  allTabElements.removeClass('active-tab active-tab-mbl');
+  allTabElements.addClass('inactive-tab inactive-tab-mbl');
+
+  if (!activeStatus) {
+    $(this).removeClass('inactive-tab-mbl');
+    $(this).addClass('active-tab-mbl');
+   }
+
   $(this).removeClass('inactive-tab');
   $(this).addClass('active-tab');
+  
   let classesArr = $(this).attr('class').split(' ');
   let numClass = classesArr.filter(classStr => classStr.includes('elem'))
   let boxClass = 'box' + numClass[0].charAt(0);
 
   // Update boxes
   let allBoxElements = $(this).closest('.accordian-container').children('.box');
+  let targetBoxElement =   $(this).closest('.accordian-container').find(`.${boxClass}`);
 
-  allBoxElements.removeClass('active-box');
-  allBoxElements.addClass('inactive-box');
-  $(this).closest('.accordian-container').find(`.${boxClass}`).removeClass('inactive-box');
-  $(this).closest('.accordian-container').find(`.${boxClass}`).addClass('active-box');
+  allBoxElements.removeClass('active-box active-box-mbl');
+  allBoxElements.addClass('inactive-box inactive-box-mbl');
+
+  if (!activeStatus) {
+    targetBoxElement.removeClass('inactive-box-mbl');
+    targetBoxElement.addClass('active-box-mbl');
+  }
+
+  targetBoxElement.removeClass('inactive-box');
+  targetBoxElement.addClass('active-box');
 })
+
 
 $('.header-bar-container').on('click', function() {
   $(this).children('.hide-item').toggleClass('inactive-item');
 });
 
+
 // View Code button
-$('#header-bar-btn').on('click', function() {
-  viewCode();
+$('#accordian-tabs-btn, #flexible-cards-btn, #header-bar-btn').on('click', function() {
+  let sectionId = $(this).parent('section').attr('id');
+  viewCode(sectionId);
 });
 
-const viewCode = () => {
-  console.log('view code');
-    var source = "<html>";
-    source += document.getElementsByTagName('html')[0].innerHTML;
-    source += "</html>";
-    source = source.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    source = "<pre>"+source+"</pre>";
+const viewCode = (id) => {
+    let source = `    <section id='${id}'>`;
+    source += document.getElementById(id).innerHTML;
+    source += '</section>';
+    source = source.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    source = '<pre>'+source+'</pre>';
     sourceWindow = window.open('','Source of page','height=800,width=800,scrollbars=1,resizable=1');
     sourceWindow.document.write(source);
     sourceWindow.document.close();
